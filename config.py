6598520fb6ddfb17e5a3647822f218354b57c770
Thanapar <ballmegane1@gmail.com>
2017-08-20 20:25:30 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,30 @@
+CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+DOCUMENTDB_HOST = 'https://education-pj.documents.azure.com:443/'
+DOCUMENTDB_KEY = 'rDHE1fEOXabawfM4zNYbOerTrWJeFQGSIWVkTAZ1VSOOtogfjbWGCaW3x2wvkKHgUA1N9sGWcO3Bf5VETZg6pA=='
+
+DOCUMENTDB_DATABASE = 'Account'
+DOCUMENTDB_COLLECTION = 'collection'
+#DOCUMENTDB_DOCUMENT = '1'
+DOCUMENTDB_NUM = ''
+DOCUMENTDB_COUNT = 0
+
+DOCUMENTDB_NAMEMAIN = ""
+DOCUMENTDB_PASSMAIN = ""
+
+DOCUMENTDB_NAME = ""
+DOCUMENTDB_FACULTY = ""
+DOCUMENTDB_PHONE = ""
+DOCUMENTDB_PASSWORD = ""
+
+'''
+def add(newVal):
+   DOCUMENTDB_NUM = newVal
+   tmp = int(DOCUMENTDB_COUNT)
+   tmp += 1
+   #DOCUMENTDB_COUNT = tmp
+   return DOCUMENTDB_NUM,tmp
+'''
+
+
